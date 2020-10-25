8c175b1b0d44866906c7aa0186ffe4b339857b4d
Vadym <vadikdeputat@gmail.com>
2020-10-25 16:09:18 +0100

    new test

--- src/test/java/app/HelloServiceTest.java
+++ src/test/java/app/HelloServiceTest.java
@@ -11,7 +11,7 @@ public class HelloServiceTest {
     private final static String WELCOME = "Hello";
 
     @Test
-    public void test_nullName_prepareGreeting_returnValue() throws Exception {
+    public void test_nullName_prepareGreeting_returnValue() {
 
 
         LangRepository mockReposetory = getMockLangReposetory();
@@ -26,14 +26,39 @@ public class HelloServiceTest {
     }
 
     @Test
-    public void test_name_prepareGreeting_returnsWithName() throws Exception {
-        HelloService serviceTest = new HelloService(new LangRepository());
+    public void test_name_prepareGreeting_returnsWithName() {
+        LangRepository mockReposetory = getMockLangReposetory();
+
+        HelloService serviceTest = new HelloService(mockReposetory);
 
         String name = "test";
 
         String result = serviceTest.prepareGreeting(name, "-1");
 
         assertEquals(WELCOME + " " + name + "!", result);
+
+    }
+
+    @Test
+    public void test_nullLang_prepareGreeting_returnsIdLang() {
+        String hola = "Hola";
+        LangRepository mockReposetory = new LangRepository() {
+            @Override
+            Optional<Language> findById(Long id) {
+                if(id.equals(HelloService.FALLBACK_LANG.getId())) {
+                    return Optional.of(new Language(null, hola, null));
+                }
+                return Optional.empty();
+            }
+        };
+
+        HelloService serviceTest = new HelloService(mockReposetory);
+
+        String name = "test";
+
+        String result = serviceTest.prepareGreeting(null, null);
+
+        assertEquals(hola + " " + HelloService.FALLBACK_NAME + "!", result);
 
     }
 
