76ddad992a72d7567b4632325a07f9a1001d8b12
Vadym <vadikdeputat@gmail.com>
2020-10-25 14:42:30 +0100

    add constructor and get/set to class Language

--- src/main/java/app/Language.java
+++ src/main/java/app/Language.java
@@ -1,5 +1,33 @@
 package app;
 
 class Language {
+    private Long id;
+    private String greetingMsg;
+    private String code;
 
+    public Language(Long id, String greetingMsg, String code) {
+        this.id = id;
+        this.greetingMsg = greetingMsg;
+        this.code = code;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getGreetingMsg() {
+        return greetingMsg;
+    }
+
+    public void setGreetingMsg(String greetingMsg) {
+        this.greetingMsg = greetingMsg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
